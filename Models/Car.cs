d59272818bba67cdee825eabd86d9eea8202c6fc
Sadullayev_o21 <muhammadziyosadullayev755@gmail.com>
2026-05-16 19:23:44 +0500

    Yangilandi' git push git

---
+++ Models/Car.cs
@@ -0,0 +1,37 @@
+namespace Transport.Models
+{
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public int Year;
+        public double FuelLevel;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\n Mashina: {Brand} {Model} ({Year}-yil)");
+            Console.WriteLine($"Yoqilg'i darajasi: {FuelLevel}%");
+        }
+
+        public void Drive(double distance)
+        {
+            double needed = distance / 10;
+            if (FuelLevel >= needed)
+            {
+                FuelLevel -= needed;
+                Console.WriteLine($"{distance} km yo'l bosildi. -{needed}% yoqilg'i.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel!");
+            }
+        }
+
+        public void Refuel(double amount)
+        {
+            FuelLevel += amount;
+            if (FuelLevel > 100) FuelLevel = 100;
+            Console.WriteLine($"{amount}% yoqilg'i qo'shildi.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Transport.Models;
+
+Car nexia = new Car();
+nexia.Brand = "Chevrolet";
+nexia.Model = "Nexia 3";
+nexia.Year = 2019;
+nexia.FuelLevel = 30;
+
+nexia.ShowInfo();
+nexia.Drive(200); 
+nexia.Refuel(15);
+nexia.ShowInfo();
+
+Console.WriteLine("\n-----------------------");
+
+Car tesla = new Car 
+{ 
+    Brand = "Tesla", 
+    Model = "Model S", 
+    Year = 2023, 
+    FuelLevel = 15 
+};
+
+tesla.ShowInfo();
+tesla.Drive(250);
+tesla.Refuel(80);
+tesla.ShowInfo();
