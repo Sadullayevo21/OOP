1a2aae76b3a389625da8aacadade7d8f050fcb27
Sadullayev_o21 <muhammadziyosadullayev755@gmail.com>
2026-05-20 18:51:47 +0500

    Program.cs Updated

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
 ﻿using Transport.Models;
 
 Car nexia = new Car();
+
 nexia.Brand = "Chevrolet";
 nexia.Model = "Nexia 3";
 nexia.Year = 2019;
-nexia.FuelLevel = 30;
+
+nexia.FuelLevel = 20;
+
+
+// Speed
+nexia.Speed = 50;
+nexia.IncreaseSpeed(30);
+nexia.DecreaseSpeed(20);
+
+
+// Destination
+nexia.Destination = "Toshkent";
 
 nexia.ShowInfo();
-nexia.Drive(200); 
-nexia.Refuel(15);
+
+
+// Drive
+nexia.Drive(200);
+
+
+// yoqilg'i qolmasa
+nexia.Destination = "Samarqand";
+
 nexia.ShowInfo();
 
-Console.WriteLine("\n-----------------------");
 
-Car tesla = new Car 
-{ 
-    Brand = "Tesla", 
-    Model = "Model S", 
-    Year = 2023, 
-    FuelLevel = 15 
-};
+// yoqilg'i quyish
+nexia.Refuel(50);
 
-tesla.ShowInfo();
-tesla.Drive(250);
-tesla.Refuel(80);
-tesla.ShowInfo();
+nexia.Destination = "Samarqand";
+
+nexia.ShowInfo();
